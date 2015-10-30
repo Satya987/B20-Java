37c24f76bc058c447e6a87cb43f56b8ba35145f8
Satya <Satya.Samal05@gmail.com>
2015-10-29 19:13:57 -0500

    Updated Java Code

---
+++ JavaProject/src/Bottom2.java
@@ -0,0 +1,19 @@
+class Top{
+
+	//public Top(String S){System.out.println("A");}
+	
+}
+
+public class Bottom2 extends Top{
+	
+	
+	//public Bottom2 (String S){System.out.println("B"); }
+	
+	public static void main( String [] arg){
+
+		
+			// TODO Auto-generated constructor stub
+		
+
+	}
+}
